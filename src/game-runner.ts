7162785d7df422d7a12b2d3575e1c624f32e59fb
Paola Reyes <paolareyes.tw@packlink.com>
2020-02-26 09:42:12 +0100

    extract to play to method
    Signed-off-by: Nelson Jumbo <njumbo@thoughtworks.com>

--- src/game-runner.ts
+++ src/game-runner.ts
@@ -7,21 +7,24 @@ export class GameRunner {
         game.add("Pat");
         game.add("Sue");
 
+        this.play(game);
+    }
+
+    private static play(game: Game) {
         let notAWinner;
         do {
 
             game.roll(Math.floor(Math.random() * 6) + 1);
-        
+
             if (Math.floor(Math.random() * 10) == 7) {
-            notAWinner = game.wrongAnswer();
+                notAWinner = game.wrongAnswer();
             } else {
-            notAWinner = game.wasCorrectlyAnswered();
+                notAWinner = game.wasCorrectlyAnswered();
             }
-        
+
         } while (notAWinner);
     }
 }
 
 GameRunner.main();
 
-  
