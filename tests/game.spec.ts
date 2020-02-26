264ba6ee62f3c58a9be3b60ea42386cbff2e7728
Asier Barrenetxea <asierba@gmail.com>
2020-02-26 09:35:39 +0100

    added golden master test
    Signed-off-by: Paola Reyes <paolareyes.tw@packlink.com>

--- tests/game.spec.ts
+++ tests/game.spec.ts
@@ -1,11 +1,15 @@
 import {GameRunner} from '../src/game-runner';
+import  * as seedrandom from 'seedrandom';
 
 describe('The test environment', () => {
-    it('should pass', () => {
-        expect(true).toBe(true);
-    });
 
-    it("should access game", function () {
-        expect(GameRunner).not.toBe(undefined);
+    it('should golden master', () => {
+        let newConsoleLog = jest.fn();
+        global.console.log = newConsoleLog;
+        for(let i=0; i<500; i++){
+            seedrandom(i.toString(), { global: true });
+            GameRunner.main();
+        }
+        expect(newConsoleLog.mock.calls).toMatchSnapshot();
     });
 });
