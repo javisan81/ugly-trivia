b97743b7d1fca5bb1da7e3fdb7a662d1c85846b9
Paola Reyes <paolareyes.tw@packlink.com>
2020-04-08 09:23:26 +0200

    Move x to different file
    Signed-off-by: Nelson Jumbo <njumbo@thoughtworks.com>

--- src/game.ts
+++ src/game.ts
@@ -1,88 +1,13 @@
-
-enum Category {
-    Rock = 'Rock',
-    Sports = 'Sports',
-    Science = 'Science',
-    Pop = 'Pop'
-}
-interface CategoryQuestionsInTheBoard {
-    category: Category;
-    positions: Array<number>;
-    questions: Array<string>;
-}
-
-class CategoryQuestionsInTheBoard2 implements CategoryQuestionsInTheBoard {
-    category: Category;
-    positions: Array<number>;
-    questions: Array<string>;
-    constructor(category: Category, positions: Array<number>, questions: Array<string>) {
-        this.category = category;
-        this.positions = positions;
-        this.questions = questions;
-    }
-}
-
-class X {
-    private popQuestions: Array<string> = [];
-    private scienceQuestions: Array<string> = [];
-    private sportsQuestions: Array<string> = [];
-    private rockQuestions: Array<string> = [];
-
-    constructor() {
-        this.initializeQuestions();
-    }
-
-    private initializeQuestions() {
-        for (let i = 0; i < 50; i++) {
-            this.popQuestions.push('Pop Question ' + i);
-            this.scienceQuestions.push('Science Question ' + i);
-            this.sportsQuestions.push('Sports Question ' + i);
-            this.rockQuestions.push(this.createRockQuestion(i));
-        }
-    }
-
-    private createRockQuestion(index: number): string {
-        return 'Rock Question ' + index;
-    }
-
-    private rockCategoryQuestion = {category: Category.Rock, positions: [], questions: this.rockQuestions};
-
-    private categoriesQuestionsInTheBoard: Array<CategoryQuestionsInTheBoard> = [
-        {category: Category.Pop, positions: [0, 4, 8], questions: this.popQuestions},
-        {category: Category.Science, positions: [1, 5, 9], questions: this.scienceQuestions},
-        {category: Category.Sports, positions: [2, 6, 10], questions: this.sportsQuestions},
-        this.rockCategoryQuestion,
-    ];
-
-    private currentCategoryInTheBoard(currentPosition: number): CategoryQuestionsInTheBoard2 {
-        const foundCategory =  this.categoriesQuestionsInTheBoard.find((categoryForPositions) =>
-            categoryForPositions.positions.includes(currentPosition));
-        return foundCategory ? foundCategory : this.rockCategoryQuestion;
-    }
-
-    public currentCategory(currentPosition: number): Category {
-        return this.currentCategoryInTheBoard(currentPosition).category;
-    }
-
-    public nextQuestion(currentPosition: number) {
-        return this.currentCategoryInTheBoard(currentPosition).questions.shift();
-    }
-}
-
+import { X } from './x';
 
 export class Game {
-
-    private boardShowsQuestion(): void {
-        console.log(this.x.nextQuestion(this.places[this.currentPlayer]));
-    }
-
     private players: Array<string> = [];
+
     private places: Array<number> = [];
     private purses: Array<number> = [];
-
     private inPenaltyBox: Array<boolean> = [];
-    private currentPlayer: number = 0;
 
+    private currentPlayer: number = 0;
     private isGettingOutOfPenaltyBox: boolean = false;
 
     private x: X;
@@ -117,6 +42,10 @@ export class Game {
             console.log('The category is ' + this.x.currentCategory(this.places[this.currentPlayer]));
             this.boardShowsQuestion();
         }
+    }
+
+    private boardShowsQuestion(): void {
+        console.log(this.x.nextQuestion(this.places[this.currentPlayer]));
     }
 
     private static rollDice(): number {
---
+++ src/x.ts
@@ -0,0 +1,69 @@
+enum Category {
+    Rock = 'Rock',
+    Sports = 'Sports',
+    Science = 'Science',
+    Pop = 'Pop'
+}
+interface CategoryQuestionsInTheBoard {
+    category: Category;
+    positions: Array<number>;
+    questions: Array<string>;
+}
+
+class CategoryQuestionsInTheBoard2 implements CategoryQuestionsInTheBoard {
+    category: Category;
+    positions: Array<number>;
+    questions: Array<string>;
+    constructor(category: Category, positions: Array<number>, questions: Array<string>) {
+        this.category = category;
+        this.positions = positions;
+        this.questions = questions;
+    }
+}
+
+export class X {
+    private popQuestions: Array<string> = [];
+    private scienceQuestions: Array<string> = [];
+    private sportsQuestions: Array<string> = [];
+    private rockQuestions: Array<string> = [];
+
+    constructor() {
+        this.initializeQuestions();
+    }
+
+    private initializeQuestions() {
+        for (let i = 0; i < 50; i++) {
+            this.popQuestions.push('Pop Question ' + i);
+            this.scienceQuestions.push('Science Question ' + i);
+            this.sportsQuestions.push('Sports Question ' + i);
+            this.rockQuestions.push(this.createRockQuestion(i));
+        }
+    }
+
+    private createRockQuestion(index: number): string {
+        return 'Rock Question ' + index;
+    }
+
+    private rockCategoryQuestion = {category: Category.Rock, positions: [], questions: this.rockQuestions};
+
+    private categoriesQuestionsInTheBoard: Array<CategoryQuestionsInTheBoard> = [
+        {category: Category.Pop, positions: [0, 4, 8], questions: this.popQuestions},
+        {category: Category.Science, positions: [1, 5, 9], questions: this.scienceQuestions},
+        {category: Category.Sports, positions: [2, 6, 10], questions: this.sportsQuestions},
+        this.rockCategoryQuestion,
+    ];
+
+    private currentCategoryInTheBoard(currentPosition: number): CategoryQuestionsInTheBoard2 {
+        const foundCategory = this.categoriesQuestionsInTheBoard.find((categoryForPositions) =>
+            categoryForPositions.positions.includes(currentPosition));
+        return foundCategory ? foundCategory : this.rockCategoryQuestion;
+    }
+
+    public currentCategory(currentPosition: number): Category {
+        return this.currentCategoryInTheBoard(currentPosition).category;
+    }
+
+    public nextQuestion(currentPosition: number) {
+        return this.currentCategoryInTheBoard(currentPosition).questions.shift();
+    }
+}
